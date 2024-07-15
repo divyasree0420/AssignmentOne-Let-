893099ac44ffc42d8f30f4901d15f87722cb97fb
divyasree0420 <bommydivyasree44506@gmail.com>
2024-07-15 12:37:58 +0530

    Add files via upload
    First Assignment of using let,const and var

---
+++ AssignOne.js
@@ -0,0 +1,53 @@
+//Scenario1
+let currentTemperature = 25; // Initial temperature
+
+
+currentTemperature = 28;       // Updated temperature
+
+
+//Scenario2
+const daysOfWeek = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday'
+];
+
+
+
+
+
+//Scenario3
+
+
+function myFunction() {
+    let localVar = 'This variable is only accessible within myFunction';
+    
+}
+
+
+
+//Scenario4
+
+const PI = 3.14159;
+
+//Scenario5
+
+const myArray = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < myArray.length; i++) {
+    
+    console.log(myArray[i]);
+}
+
+
+// Scenario6
+
+const userProfile = {
+    name: 'Pavan', 
+    dob: '1990-01-01', 
+    email: 'pavan@gmail.com' }
+    console.log(name,dob,email);
